44d49b0475c330ac380590fed0e0d76366b0e120
gibril1435 <156919577+gibril1435@users.noreply.github.com>
2024-01-18 20:27:01 +0700

    Update kakulator.c

--- kakulator.c
+++ kakulator.c
@@ -2,6 +2,7 @@
 
 int main() {
     int angka1, angka2, hasil;
+    char operasi;
 
     printf("Masukkan angka pertama: ");
     scanf("%d", &angka1);
@@ -9,9 +10,24 @@ int main() {
     printf("Masukkan angka kedua: ");
     scanf("%d", &angka2);
 
-    hasil = angka1 - angka2;
+    printf("Pilih operasi (+ untuk pertambahan, - untuk pengurangan): ");
+    scanf(" %c", &operasi);
 
-    printf("Hasil pertambahan: %d\n", hasil);
+    switch (operasi) {
+        case '+':
+            hasil = angka1 + angka2;
+            printf("Hasil pertambahan: %d\n", hasil);
+            break;
+
+        case '-':
+            hasil = angka1 - angka2;
+            printf("Hasil pengurangan: %d\n", hasil);
+            break;
+
+        default:
+            printf("Operasi tidak valid. Silakan pilih + atau -.\n");
+            break;
+    }
 
     return 0;
 }
