04cbe101ea76b0616f1fdd88ad2493351359a43f
gibril1435 <gibril1435@gmail.com>
2024-01-18 19:59:45 +0700

    second commit

--- kakulator.c
+++ kakulator.c
@@ -3,18 +3,16 @@
 int main() {
     int angka1, angka2, hasil;
 
-    // Input dua angka
     printf("Masukkan angka pertama: ");
     scanf("%d", &angka1);
 
     printf("Masukkan angka kedua: ");
     scanf("%d", &angka2);
 
-    // Melakukan operasi pengurangan
     hasil = angka1 - angka2;
 
-    // Menampilkan hasil
-    printf("Hasil pengurangan: %d\n", hasil);
+    printf("Hasil pertambahan: %d\n", hasil);
 
     return 0;
 }
+s
